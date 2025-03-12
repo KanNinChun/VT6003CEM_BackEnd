4b2b0dca2a96870158a6d4edff766480b63e840f
KanNinChun <kanninchun@gmail.com>
2025-03-12 10:56:37 +0800

    Fixed package version, added txt for starter  document

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import Koa from "koa";
 import Router, {RouterContext} from "koa-router";
 import logger from "koa-logger";
 import json from "koa-json";
+
 const app: Koa = new Koa();
 const router: Router = new Router();
 router.get('/', async (ctx: RouterContext, next: any) => {
